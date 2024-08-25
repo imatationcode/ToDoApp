ed079872ec24037c2a9beb854be221a9a496dad8
imatationcode <shivaasus531@gmail.com>
2024-08-25 11:37:17 +0530

    Remaining

--- Todoyee/Controller/ToDoViewController.swift
+++ Todoyee/Controller/ToDoViewController.swift
@@ -1,40 +1,55 @@
 //
 //  ViewController.swift
 //  Todoyee
-//
 //  Created by Shivakumar Harijan on 17/08/24.
 //
 
 import UIKit
 
 class ToDoViewController: UITableViewController {
-    var taskListArray:[String] = []
-    let defaultsVar = UserDefaults.standard
+    
+    var taskListArray = [TaskModalClass]()
+    var newTask : TaskModalClass!
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("ToDoDataFile.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.barTintColor = .red
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewTask))
         addButton.tintColor = .black
         navigationItem.rightBarButtonItem = addButton
-        taskListArray = defaultsVar.array(forKey: "TaskList") as? [String] ?? ["New Task"]
+        print(dataFilePath!)
+        loadTasks()
+    }
+    
+    //MARK: to Load the Presistant data on device loadTasks
+    func loadTasks() {
+        if let tasksData = try? Data(contentsOf: dataFilePath!) {
+            let myDecoer = PropertyListDecoder()
+            do {
+                self.taskListArray = try myDecoer.decode([TaskModalClass].self, from: tasksData)
+            } catch {
+                print("Error while decoding\(error)")
+            }
+        }
     }
     
     @objc func addNewTask() {
         var taskTitleTextField = UITextField()
         let alert = UIAlertController(title: "New Task", message: "", preferredStyle: .alert)
-        let cancelAction = UIAlertAction(title: "Add This", style: .default) { addAction in
-            print(taskTitleTextField.text!)
-            self.taskListArray.append(taskTitleTextField.text!)
-            self.tableView.reloadData()
-            self.defaultsVar.set(self.taskListArray, forKey: "TaskList")
-        }
-        
         
         alert.addTextField { alertTextField in
             alertTextField.placeholder = "Add Neew Task"
             taskTitleTextField = alertTextField
         }
-        alert.addAction(cancelAction)
+        
+        let addTaskButton = UIAlertAction(title: "Add This", style: .default) { addAction in
+            self.newTask = TaskModalClass()
+            self.newTask?.taskName = taskTitleTextField.text!
+            self.taskListArray.append(self.newTask!)
+            self.saveIteamsAndState()
+        }
+        alert.addAction(addTaskButton)
         self.present(alert, animated: true)
     }
     
@@ -45,19 +60,34 @@ class ToDoViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoIdentifier", for: indexPath)
         let reversedIndex = taskListArray.count - 1 - indexPath.row
-        cell.textLabel?.text = taskListArray[reversedIndex]
+        
+        cell.textLabel?.text = taskListArray[reversedIndex].taskName
+        
+        cell.accessoryType = taskListArray[reversedIndex].isCompletedTask == true ? .checkmark : .none
+        
         return cell
     }
+    
     //MARK: table view delegate methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+        let reversedIndex = taskListArray.count - 1 - indexPath.row
+        taskListArray[reversedIndex].isCompletedTask.toggle()
+        saveIteamsAndState()
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK: saveIteamsN Status Function
+    
+    func saveIteamsAndState() {
+        let myEncoder = PropertyListEncoder()
+        do {
+            let data = try myEncoder.encode(taskListArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error While Encoding")
+        }
+        tableView.reloadData()
+    }
 
-}
+}//class End
 
--- Todoyee/Modal/TaskModalClass.swift
+++ Todoyee/Modal/TaskModalClass.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+struct TaskModalClass: Codable {
+    var taskName : String?
+    var isCompletedTask : Bool = false
+}
